36018e41e2ed634519a3b1809c8d577562d0a663
kawasaki.taiga <kawasaki.taiga@raccoon.ne.jp>
2023-07-30 04:13:00 +0900

    solved typical90 003, 004

--- typical90/src/bin/003.rs
+++ typical90/src/bin/003.rs
@@ -1,5 +1,46 @@
-use petgraph::{Directed, Graph, visit::Dfs};
+use std::collections::VecDeque;
+
 use proconio::{fastout, input};
+
+
+#[derive(Debug)]
+struct Node {
+    edge: Vec<usize>,
+}
+#[derive(Debug)]
+struct Graph {
+    node: Vec<Node>,
+}
+impl Graph {
+    fn new(n: usize) -> Graph {
+        let nodes: Vec<Node> = (0..n).map(|_| Node { edge: vec![] }).collect();
+        Graph { node: nodes }
+    }
+    fn add_edge(&mut self, a: usize, b: usize) {
+        self.node[a].edge.push(b);
+        self.node[b].edge.push(a);
+    }
+}
+
+fn bfs(g: &Graph, start: usize) -> Vec<isize> {
+    let mut queue: VecDeque<usize> = VecDeque::new();
+    queue.push_back(start);
+    let mut distance = vec![-1 as isize; g.node.len()];
+    distance[start] = 0;
+    while let Some(node_index) = queue.pop_front() {
+        let node = &g.node[node_index];
+
+        for &next_node in node.edge.iter() {
+            if distance[next_node] != -1 {
+                continue;
+            } else {
+                distance[next_node] = distance[node_index] + 1;
+            }
+            queue.push_back(next_node);
+        }
+    }
+    distance
+}
 
 #[fastout]
 fn main() {
@@ -7,12 +48,14 @@ fn main() {
         n: usize,
         a: [(usize, usize);n-1]
     }
-    let graph = Graph::<usize, (), Directed, usize>::from_edges(a.iter().map(|(x, y)| {
-        (x-1, y-1)
-    }).collect::<Vec<(usize, usize)>>());
+    let mut g = Graph::new(n);
+    a.iter().for_each(|&(x, y)| {
+        g.add_edge(x - 1, y - 1);
+    });
+    let bfs_result = bfs(&g, 0);
+    let max = bfs_result.iter().enumerate().max_by(|x, y| {
+        x.1.cmp(y.1)
+    }).unwrap();
 
-    let mut dfs = Dfs::new(&graph, graph.getNodeIndex(0).unwrap());
-    while let Some(nx) = dfs.next(&graph) {
-        println!("{}", nx + 1);
-    }
+    println!("{:?}", bfs(&g, max.0).iter().max().unwrap()+1);
 }
--- typical90/src/bin/004.rs
+++ typical90/src/bin/004.rs
@@ -3,6 +3,24 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        h: usize,
+        w: usize,
+        a: [[usize; w]; h],
     }
-    todo!()
+    let row_sum = a.iter().map(|row| {
+        row.iter().sum()
+    }).collect::<Vec<usize>>();
+    let col_sum = (0..w).map(|i| {
+        (0..h).map(|j| a[j][i]).sum::<usize>()
+    }).collect::<Vec<usize>>();
+
+    for i in 0..h {
+        for j in 0..w {
+            print!("{}", row_sum[i] + col_sum[j] - a[i][j]);
+            if j != w - 1 {
+                print!(" ");
+            }
+        }
+        println!();
+    }
 }
