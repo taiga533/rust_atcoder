bcc337cfc4e7de0c55b61fbd28b13bc311d08392
kawasaki.taiga <kawasaki.taiga@raccoon.ne.jp>
2023-08-01 23:11:29 +0900

    solved typical90 007 008

--- typical90/src/bin/007.rs
+++ typical90/src/bin/007.rs
@@ -1,8 +1,36 @@
 use proconio::{fastout, input};
+use superslice::*;
 
 #[fastout]
 fn main() {
     input! {
+        n: usize,
+        mut classes: [usize;n],
+        wait_students: usize,
+        mut students: [usize;wait_students]
     }
-    todo!()
+
+    classes.sort();
+
+    for student in students {
+        let min_over_idx = classes.lower_bound(&student);
+        if min_over_idx == n {
+            println!("{}", student - classes[n-1]);
+            continue;
+        }
+        let min_over = classes[min_over_idx] as isize;
+        let student = student as isize;
+        if min_over_idx == 0 {
+            println!("{}", min_over - student);
+            continue;
+        }
+        let max_under = classes[min_over_idx-1]as isize;
+
+        if min_over - student < student - max_under {
+            println!("{}", min_over - student);
+        } else {
+            println!("{}", student - max_under);
+        }
+
+    }
 }
--- typical90/src/bin/008.rs
+++ typical90/src/bin/008.rs
@@ -1,8 +1,29 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Chars};
 
 #[fastout]
 fn main() {
     input! {
+        _: usize,
+        s: Chars,
     }
-    todo!()
+    let atcoder = "atcoder";
+    let mut dp = vec![vec![0; s.len()+1]; atcoder.chars().count()+1];
+
+    dp[0] = dp[0].iter().map(|_| 1).collect();
+
+    let chars = atcoder.chars().collect::<Vec<char>>();
+
+    for i in 1..=atcoder.chars().count() {
+        // println!("{:?}", i);
+        for j in 1..=s.len() {
+            if s[j-1] == chars[i-1] {
+                dp[i][j] = (dp[i-1][j-1] + dp[i][j-1]) % 1_000_000_007;
+            }
+            else {
+                dp[i][j] = dp[i][j-1];
+            }
+        }
+    }
+
+    println!("{:?}", dp[dp.len()-1][dp[0].len()-1]);
 }
