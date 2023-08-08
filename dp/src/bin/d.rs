57b89862d67355095363d7a9739c578c31bd3f5d
kawasaki.taiga <kawasaki.taiga@raccoon.ne.jp>
2023-08-09 01:50:01 +0900

    solved dp d

--- dp/src/bin/d.rs
+++ dp/src/bin/d.rs
@@ -3,6 +3,22 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        n: usize,
+        w: usize,
+        wv: [(usize, usize); n],
     }
-    todo!()
+
+    let mut dp = vec![vec![0; w + 1]; n + 1];
+
+    for i in 0..n {
+        let (weight, value) = wv[i];
+        for j in 0..w+1 {
+            if j >= weight {
+                dp[i+1][j] = std::cmp::max(dp[i][j-weight] + value, dp[i][j]);
+            } else {
+                dp[i+1][j] = dp[i][j];
+            }
+        }
+    }
+    println!("{:?}", dp[n][w]);
 }
